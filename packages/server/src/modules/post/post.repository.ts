75a0dce054c157d2b960ec337c2a9c5b07185c01
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-09 17:47:30 +0900

    feat: post repository

--- packages/server/src/modules/post/post.repository.ts
+++ packages/server/src/modules/post/post.repository.ts
@@ -4,4 +4,40 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class PostRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  findPosts() {
+    return this.prisma.post.findMany();
+  }
+
+  findPostById(postId: number) {
+    return this.prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+    });
+  }
+
+  createPost(
+    title: string,
+    content: string,
+    published: boolean,
+    authorId: number,
+  ) {
+    return this.prisma.post.create({
+      data: {
+        title,
+        content,
+        published,
+        authorId,
+      },
+    });
+  }
+
+  deletePostById(postId: number) {
+    return this.prisma.post.delete({
+      where: {
+        id: postId,
+      },
+    });
+  }
 }
